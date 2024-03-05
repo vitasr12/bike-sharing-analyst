70216c4bfac6a442d8cdefe4fe70297b9578d17e
vita sariani <90192195+vitasr12@users.noreply.github.com>
2024-03-05 11:26:24 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='dark')
 
-all_df = pd.read_csv("/dashboard/all_data.csv")
+
+path = os.path.dirname(_file_)
+my_file = path+'/all_data.csv'
+all_df = pd.read_csv(my_file)
 
 
 st.header("Project Analasisis Data Bike Sharing Dataset:sparkles:")
